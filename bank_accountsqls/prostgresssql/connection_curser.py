1b658a3f5fa586ebdcd9984425b1ac07c4b5ba01
sahand <sahandha1391@gmail.com>
2025-02-23 12:36:02 -0330

    interpenture

--- bank_accountsqls/prostgresssql/connection_curser.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-import psycopg2.extras
-class Cots:
-    def __init__(self):
-
-        self.connection = psycopg2.connect(host='127.0.0.1',database='bank_account_pgadmin_version',port=5432,user='postgres',password='sahand91')
-        self.cursor = self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
