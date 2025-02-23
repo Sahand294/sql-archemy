fbc7ec7340537b882b626c8e119a41eee209c2e5
sahand <sahandha1391@gmail.com>
2025-02-23 16:45:47 -0330

    bug fixed

---
+++ learning/sql_in_python/sqllite/transactions.py
@@ -0,0 +1,13 @@
+import sqlite3
+connection = sqlite3.connect('mydatabase.db')
+cursor = connection.cursor()
+try:
+    cursor.execute("BEGIN TRANSACTION")
+    cursor.execute("INSERT INTO user_temth(name,email) VALUES('you','hey')")
+    cursor.execute("INSERT INTO user_temth(name,email) VALUES('you','hey')")
+    connection.commit()
+except sqlite3.IntegrityError as E:
+    print(E)
+    connection.rollback()
+finally:
+    connection.close()
